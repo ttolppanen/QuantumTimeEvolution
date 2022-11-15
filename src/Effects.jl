863624a9be53259fccbb6b9d7d50ebb03deadb1f
ttolppanen <taneli.tolppanen@gmail.com>
2022-11-15 16:36:53 +0200

    kwarg support for measuresitesrandomly

--- src/Effects.jl
+++ src/Effects.jl
@@ -8,11 +8,11 @@ MsrOpType = Union{MsrOpMatrixType, MsrOpITensorType}
 
 #msr_prob : measurement probability; the probability that a single site will be measured
 #Use this as an effect: measureeffect!(state) = measuresitesrandomly(state, msrop, msr_prob)
-function measuresitesrandomly!(state::StateType, msrop::MsrOpType, msr_prob::Real)
+function measuresitesrandomly!(state::StateType, msrop::MsrOpType, msr_prob::Real; kwargs...) #kwargs for measuresite!
     L = length(msrop)
     for i in 1:L
         if rand(Float64) < msr_prob
-            measuresite!(state, msrop, i)
+            measuresite!(state, msrop, i; kwargs...) #kwargs are for ITensors.apply
         end
     end
 end

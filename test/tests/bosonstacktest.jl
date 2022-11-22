7b11ea95b5ab671a1e2a50984c6082651adf67b6
ttolppanen <taneli.tolppanen@gmail.com>
2022-11-22 14:48:35 +0200

    fixed bosonstack test

--- test/tests/bosonstacktest.jl
+++ test/tests/bosonstacktest.jl
@@ -3,7 +3,7 @@
 d = 4; L = 4; N = 3
 U = 50
 J = 2
-J_e = (N - 1) * N / factorial(N - 1) * (J/U)^(N - 1) * J
+J_e = N / factorial(N - 1) * (J/U)^(N - 1) * J
 dt = 1; t = 2 * pi * U
 target = 2
 state0 = bosonstack(N, L, target)
@@ -16,10 +16,10 @@ r_mps = mpsevolve_bosehubbard(mps0, dt, t; U, J)
 
 n = singlesite_n(d, L, target)
 x_t = (0:dt:t) / (pi / J_e)
-pl = plot(x_t, expval(r_exact, n), label="exact", title = "U = $(U), J = $(J)")
+pl = plot(x_t, expval(r_exact, n), label="exact", title = "U = $(U), J = $(J) (MPS fails because dt too small)")
 plot!(pl, x_t, expval(r_krylov, n), label="krylov")
 plot!(pl, x_t, expval(r_mps, "N"; sites=3), label="mps")
-plot!(pl, x_t, x -> N * (cos(x * pi / 2)^2), label="analytical", linestyle = :dash)
+plot!(pl, x_t, x -> N * (cos(x * pi)^2), label="analytical", linestyle = :dash)
 saveplot(pl, "bosonstack")
 @test true
 

a7de4df78bd858f8b0305dd71f59d58b72164459
ttolppanen <taneli.tolppanen@gmail.com>
2022-12-02 14:53:21 +0200

    multithreading testing to branch

--- test/performance/mpsprofile.jl
+++ test/performance/mpsprofile.jl
@@ -6,7 +6,6 @@ using ITensors
 function mipt(d, L, dt, t, traj, prob)
     mps0 = zeroonemps(d, L)
     gates = bosehubbardgates(siteinds(mps0), dt)
-    @show siteinds(mps0)
     op_to_msr = nop(d)
     msrop = measurementoperators(op_to_msr, siteinds(mps0))
     res = []
@@ -20,4 +19,4 @@ function mipt(d, L, dt, t, traj, prob)
     return prob, res
 end
 
-@profview @time mipt(2, 4, 0.1, 10.0, 5, 0.01:0.01:0.3);
+@time mipt(2, 4, 0.6, 10.0, 100, 0.1:0.1:0.1);
